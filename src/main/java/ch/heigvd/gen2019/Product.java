5278721d0f2112e4be6300167f66561aef9533eb
G-Houlmann <gildas.houlmann@gmail.com>
2020-05-23 13:25:02 +0200

    bad smell: feature envy, refactor: move method getProductContent to Product

--- src/main/java/ch/heigvd/gen2019/Product.java
+++ src/main/java/ch/heigvd/gen2019/Product.java
@@ -36,7 +36,7 @@ public class Product {
         return currency;
     }
 
-    String getSizeFor() {
+    private String getSizeFor() {
         switch (getSize()) {
             case 1:
                 return "XS";
@@ -55,7 +55,7 @@ public class Product {
         }
     }
 
-    String getColorFor() {
+    private String getColorFor() {
         switch (getColor()) {
             case 1:
                 return "blue";
@@ -66,5 +66,28 @@ public class Product {
             default:
                 return "no color";
         }
+    }
+
+    public void getProductContent(StringBuffer sb) {
+        sb.append("{");
+        sb.append("\"code\": \"");
+        sb.append(getCode());
+        sb.append("\", ");
+        sb.append("\"color\": \"");
+        sb.append(getColorFor());
+        sb.append("\", ");
+
+        if (getSize() != SIZE_NOT_APPLICABLE) {
+            sb.append("\"size\": \"");
+            sb.append(getSizeFor());
+            sb.append("\", ");
+        }
+
+        sb.append("\"price\": ");
+        sb.append(getPrice());
+        sb.append(", ");
+        sb.append("\"currency\": \"");
+        sb.append(getCurrency());
+        sb.append("\"}, ");
     }
 }
