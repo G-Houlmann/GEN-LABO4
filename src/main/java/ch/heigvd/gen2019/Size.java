c0090266a24093a9e2889d8fa3b3203b7cd59b6f
Eric Broutba <eric.bousbaa@hotmail.com>
2020-05-23 16:23:27 +0200

    bad smell: switch statements, refactor: Replace Type Code with Class

---
+++ src/main/java/ch/heigvd/gen2019/Size.java
@@ -0,0 +1,21 @@
+package ch.heigvd.gen2019;
+
+public enum Size {
+    XS("XS"),
+    S("S"),
+    M("M"),
+    L("L"),
+    XL("XL"),
+    XXL("XXL"),
+    NO_SIZE("Invalid size");
+
+    private String size;
+    Size(String size){
+        this.size = size;
+    }
+
+    @Override
+    public String toString() {
+        return "\"" + size + "\"";
+    }
+}
